fd7beb3cf79367000b337851d06fa8ba762f75f6
Dima <youremail@domain.com>
2021-11-10 22:04:07 +0300

    End Lesson

--- DjApi/lesson3.py
+++ DjApi/lesson3.py
@@ -4,13 +4,31 @@ conn = psycopg2.connect(dbname='tdv', user='tdv', password='123', host='localhos
 
 cursor = conn.cursor() # создаем обьект курсора
 
-# sql = '''SELECT * FROM main_auto WHERE brand_id in (SELECT ID FROM main_brand WHERE name in ('Лада'));
-# '''
-# sql2 = '''SELECT * FROM main_auto WHERE brand_id in (SELECT ID FROM main_brand WHERE name ='Лада' or name='БМВ');
-# '''
+sql = '''SELECT * FROM main_auto WHERE brand_id in (SELECT ID FROM main_brand WHERE name in ('Лада'));
+'''
+sql2 = '''SELECT * FROM main_auto WHERE brand_id in (SELECT ID FROM main_brand WHERE name ='Лада' or name='БМВ');
+'''
 sql3 = '''SELECT * FROM main_auto WHERE brand_id in (SELECT ID FROM main_brand WHERE name NOT IN ('Лада'));
 '''
-cursor.execute(sql3)  # вставляем SQl код
+sql4 = '''SELECT * FROM main_auto WHERE price BETWEEN 15 AND 50;
+'''
+sql5 = '''SELECT * FROM main_auto WHERE name LIKE 'Гра%';
+'''
+#sql6 = '''SELECT round(AVG(price),2) AS avg_price FROM main_auto;
+#'''
+sql7 = '''SELECT STRING_AGG (name,' ') FROM main_auto;
+'''
+sql8 = '''SELECT brand_id, COUNT(*) AS ModelCount FROM main_auto GROUP BY brand_id ORDER BY count(brand_id);
+'''
+
+# cursor.execute(sql)  # вставляем SQl код
+# cursor.execute(sql2)  # вставляем SQl код
+# cursor.execute(sql3)  # вставляем SQl код
+# cursor.execute(sql4)  # вставляем SQl код
+# cursor.execute(sql5)  # вставляем SQl код
+#cursor.execute(sql6)  # вставляем SQl код
+#cursor.execute(sql7)  # вставляем SQl код
+cursor.execute(sql8)  # вставляем SQl код
 print(cursor.fetchall())
 conn.commit()  # делаем commit
 conn.close()
